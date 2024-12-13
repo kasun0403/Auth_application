1977e4f3a91edefda259614d9c20616d3dc14578
kasun0403 <ktharanga8@gmail.com>
2024-12-13 12:13:25 +0900

    login with email and password

---
+++ lib/pages/login.dart
@@ -0,0 +1,45 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController email = TextEditingController();
+  TextEditingController password = TextEditingController();
+
+  signIn() async {
+    await FirebaseAuth.instance
+        .signInWithEmailAndPassword(email: email.text, password: password.text);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Log in"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: email,
+              decoration: const InputDecoration(hintText: "Enter email"),
+            ),
+            TextField(
+              controller: password,
+              decoration: const InputDecoration(hintText: "Enter passsword"),
+            ),
+            ElevatedButton(
+                onPressed: () => signIn(), child: const Text("log in"))
+          ],
+        ),
+      ),
+    );
+  }
+}
